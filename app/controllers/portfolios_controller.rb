ee5da90edc9192b3ac0d4a069e85b48db6995b7b
karthik <karthickrvpm@gmail.com>
2021-06-11 11:51:19 +0530

    error in edit ,added edit in portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,27 +1,23 @@
 class PortfoliosController < ApplicationController
 	def index
 		@portfolio_items=Portfolio.all 
-<<<<<<< HEAD
 	end   
 	def new 
 		@portfolio_item=Portfolio.new
 	end 
 	def create
-   @portfolio_item=Portfolio.new(params.require(:portfolio).permit(:title,:subtitle, :body))
+    	@portfolio_item=Portfolio.new(params.require(:portfolio).permit(:title,:subtitle, :body))
 
-    respond_to do |format|
-      if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: "portfilo was successfully created." }
-    
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
-    end
-  end
-=======
-	end  
-	def new 
-
+	    respond_to do |format|
+	      if @portfolio_item.save
+	        format.html { redirect_to portfolios_path, notice: "portfilo was successfully created." }
+	    
+	      else
+	        format.html { render :new, status: :unprocessable_entity }
+	      end
+	    end
 	end 
->>>>>>> 93874fa1818b045af7f209613a8e5b0c52be4cec
+	def edit 
+		@portfolio_item=Portfolio.find(params[:id])
+	end 
 end
