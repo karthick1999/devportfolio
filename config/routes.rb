5b90b32cd0cf0b39a003129f6194a8ba390054a3
karthik <karthickrvpm@gmail.com>
2021-06-11 15:35:07 +0530

    routes change in portolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
- 
+ resources :portfolios
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- get 'portfolios/new'
-  get 'pages/edit'
-  get 'pages/create'
-  resources :portfolios
+ get 'pages/home'
+  get 'about' ,to:'pages#home'
+  get 'contact', to:'pages#contact'
+  resources :blogs
+  root to:'pages#home'
 end
