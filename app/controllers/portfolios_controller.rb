22021572e6f292a6dd214a9ada826a17fb0b34d3
karthik <karthickrvpm@gmail.com>
2021-06-11 12:13:19 +0530

    edit method succesful

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -20,4 +20,14 @@ class PortfoliosController < ApplicationController
 	def edit 
 		@portfolio_item=Portfolio.find(params[:id])
 	end 
+	def update
+	@portfolio_item=Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title,:subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: "portfolio  was successfully updated." }
+      else
+        format.html { render :edit}
+      end
+    end
+  end
 end
